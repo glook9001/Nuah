f34b31c1728ca0c94c82230452cb15b6e64320dc
glook900 <glook900@users.noreply.github.com>
2026-07-29 18:03:16 +0300

    Make NuahJVM registration test release-safe

--- nuah/tests/nuah_jvm_registration_test.c
+++ nuah/tests/nuah_jvm_registration_test.c
@@ -2,22 +2,30 @@
 
 #include "jvm/jni.h"
 
-#include <assert.h>
+#include <stddef.h>
 
 static void registered_callback(void) {}
 
 int main(void) {
   NuahJvm* runtime = nuah_jvm_create();
-  assert(runtime);
+  if (!runtime) return 1;
 
   JavaVM* vm = (JavaVM*)nuah_jvm_java_vm(runtime);
-  assert(vm);
+  if (!vm) {
+    nuah_jvm_destroy(runtime);
+    return 1;
+  }
   JNIEnv* env = NULL;
-  assert((*vm)->GetEnv(vm, (void**)&env, JNI_VERSION_1_6) == JNI_OK);
-  assert(env);
+  if ((*vm)->GetEnv(vm, (void**)&env, JNI_VERSION_1_6) != JNI_OK || !env) {
+    nuah_jvm_destroy(runtime);
+    return 1;
+  }
 
   jclass activity = (*env)->FindClass(env, "com/google/androidgamesdk/GameActivity");
-  assert(activity);
+  if (!activity) {
+    nuah_jvm_destroy(runtime);
+    return 1;
+  }
   union {
     void (*function)(void);
     void* pointer;
@@ -25,19 +33,24 @@ int main(void) {
   const JNINativeMethod methods[] = {{"onKeyDownNative",
                                       "(JLandroid/view/KeyEvent;)Z",
                                       callback.pointer}};
-  assert((*env)->RegisterNatives(env, activity, methods, 1) == JNI_OK);
+  if ((*env)->RegisterNatives(env, activity, methods, 1) != JNI_OK) {
+    nuah_jvm_destroy(runtime);
+    return 1;
+  }
 
-  assert(nuah_jvm_find_registered_native(
-             runtime, "com/google/androidgamesdk/GameActivity",
-             "onKeyDownNative", "(JLandroid/view/KeyEvent;)Z") ==
-         callback.pointer);
-  assert(nuah_jvm_find_registered_native(
-             runtime, "com/google/androidgamesdk/GameActivity",
-             "onKeyDownNative", "(J)Z") == NULL);
-  assert(nuah_jvm_find_registered_native(
-             runtime, "com/google/androidgamesdk/GameActivity", "onKeyUpNative",
-             "(JLandroid/view/KeyEvent;)Z") == NULL);
+  const int valid = nuah_jvm_find_registered_native(
+                        runtime, "com/google/androidgamesdk/GameActivity",
+                        "onKeyDownNative", "(JLandroid/view/KeyEvent;)Z") ==
+                    callback.pointer;
+  const int wrong_signature = nuah_jvm_find_registered_native(
+                                  runtime,
+                                  "com/google/androidgamesdk/GameActivity",
+                                  "onKeyDownNative", "(J)Z") == NULL;
+  const int wrong_method = nuah_jvm_find_registered_native(
+                               runtime, "com/google/androidgamesdk/GameActivity",
+                               "onKeyUpNative",
+                               "(JLandroid/view/KeyEvent;)Z") == NULL;
 
   nuah_jvm_destroy(runtime);
-  return 0;
+  return valid && wrong_signature && wrong_method ? 0 : 1;
 }
